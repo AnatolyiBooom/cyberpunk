4ab050e68168ccd6c9c70b7383a723cc7926c928
Анатолий Ведерников <112936302+AnatolyiBooom@users.noreply.github.com>
2023-07-20 22:23:48 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  speed: 7000,
+  autoplay: {
+
+    delay: 2,
+    disableOnInteraction: false,
+  },
+  loop: true
+});
